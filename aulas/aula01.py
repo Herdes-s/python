51f683e5e869e9ae902bc2da3f5a71a90d390f4c
Herdes-s <soaressantos02000@gmail.com>
2025-08-11 13:29:05 -0300

    aula1

--- aulas/aula01.py
+++ aulas/aula01.py
@@ -1,7 +1,19 @@
 print('hello world!')
 
+x = 10
+nome = 'aluno'
+nota = 5.6
+fez_inscrição = True
+
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(fez_inscrição))
+
+
 nome = input('Nome:')
 idade = input('Idade:')
 nota = input('Notas:')
+
 
 print(F'Olá {nome}, vc tem {idade}anos e sua nota é {nota}.')
